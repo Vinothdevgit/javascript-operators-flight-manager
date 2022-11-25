9fb9c35e69d0ba3c967d9794fa44944a728fc142
Vinothdevgit <vinoth51115@gmail.com>
2022-11-25 23:22:38 +0800

    Update passengers.js

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,19 @@
 function Passengers() {
+    function checkFlightCapacity(flightCapacity,passengersArray){
+        let passengers = 0;
+        for(let passenger of passengersArray){
+            passengers+=passenger;
+        }
+        if(passengers <= flightCapacity){
+            return passengers;
+        }
+        else
+            throw new Error("Flight capacity ("+ flightCapacity+") exceeded. You have "+passengers);
+
+    }
+
+    return {checkFlightCapacity};
 
 }
+module.exports= Passengers();
 
